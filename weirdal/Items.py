cf2b225c68f83e6cc2720c3124a303ea40ef2b0d
Ellie53 <ellie53real@gmail.com>
2026-09-16 16:19:44 +0100

    Add extra copyright line to the header in all .py files

--- weirdal/Items.py
+++ weirdal/Items.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2026 Benny Dreamly.
+# Copyright (c) 2026 Ellie53
 
 from BaseClasses import Item, ItemClassification
 
